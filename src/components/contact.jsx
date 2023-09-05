341d963af123701e62b9a7be6c7796b505ea94db
liorG1 <lior.getahun4@gmail.com>
2023-09-05 17:39:31 +0300

    s

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,9 +6,9 @@ export default function Contact(){
     return(
         <Flex direction={['column','row']} >
             <Image src='https://qph.cf2.quoracdn.net/main-qimg-a60584595d47ece95e9c53714c16b3b1-lq'/>
-            <Flex  direction='column' borderRadius='10px' bg={'#CBB26A'} justifyContent='space-around' alignItems='center'>
+            <Flex  direction='column' borderRadius='10px' bg={'#CBB26A'}  alignItems='center'>
                 <Text fontSize='3em' font='bold'>Contact us </Text>
-                <Flex direction='row' >
+                <Flex direction='row'gap={'5px'} >
                     <Flex direction='column' gap={'5px'}>
                     <Input borderButtom='black' placeholder='Full Name'/>
                     <Input type='email' borderButtom='black' placeholder='E- mail'/>
@@ -17,8 +17,8 @@ export default function Contact(){
                     </Flex>
 <Flex direction='column'>
 
-                    <Flex direction='column'><Text>Contact</Text><Text>styleShoesByLior@gmail.com</Text></Flex>
-                    <Flex direction='column'><Text>Based In</Text><Text>Rehovot, Israel</Text></Flex>         
+                    <Flex direction='column'><Text  font='bold'>Contact</Text><Text>styleShoesByLior@gmail.com</Text></Flex>
+                    <Flex direction='column'><Text  font='bold'>Based In</Text><Text>Rehovot, Israel</Text></Flex>         
 </Flex>
                 </Flex>
             </Flex>
