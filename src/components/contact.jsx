e32f82b2b2ca2e4efa2878c95b17942bad9098a6
liorG1 <lior.getahun4@gmail.com>
2023-09-05 16:30:11 +0300

    contact

---
+++ src/components/contact.jsx
@@ -0,0 +1,14 @@
+
+import {Flex,Text,Image} from 'react'
+
+export default function Contact(){
+
+    return(
+        <Flex>
+            <Image src='https://qph.cf2.quoracdn.net/main-qimg-a60584595d47ece95e9c53714c16b3b1-lq'/>
+            <Text>Contact us </Text>
+        
+        </Flex>
+    )
+}
+
