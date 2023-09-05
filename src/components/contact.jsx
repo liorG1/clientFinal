caef8a5e19e4b0bcfa60678694ac458e370826b1
liorG1 <lior.getahun4@gmail.com>
2023-09-05 17:20:10 +0300

    x

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,21 +6,21 @@ export default function Contact(){
     return(
         <Flex>
             <Image src='https://qph.cf2.quoracdn.net/main-qimg-a60584595d47ece95e9c53714c16b3b1-lq'/>
-            <Flex  direction='row' bg={'#CBB26A'} >
+            <Flex  direction='column' bg={'#CBB26A'} >
                 <Text fontSize='3em' font='bold'>Contact us </Text>
-                <Box>
+                <Felx direction='row' >
                     <Flex direction='column' gap={'5px'}>
                     <Input borderButtom='black' placeholder='Full Name'/>
                     <Input type='email' borderButtom='black' placeholder='E- mail'/>
                     <Input type='Message' borderButtom='black' placeholder='Message'/>  
                     <Button backGroundColor='#CBB26A' width='100%'>Contact us</Button>
                     </Flex>
-<Box>
+<Flex direction='column'>
 
                     <Flex><Text>Contact</Text><Text>styleShoesByLior@gmail.com</Text></Flex>
                     <Flex><Text>Based In</Text><Text>Rehovot, Israel</Text></Flex>         
-</Box>
-                </Box>
+</Flex>
+                </Felx>
             </Flex>
         
         </Flex>
