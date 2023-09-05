9749d8ea21cd50b1dd668a79f42ecf18be38dc19
liorG1 <lior.getahun4@gmail.com>
2023-09-05 17:27:47 +0300

    C

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,7 +4,7 @@ import {Flex,Text,Image ,Grid,GridItem,Input,Button,Box} from '@chakra-ui/react'
 export default function Contact(){
 
     return(
-        <Flex>
+        <Flex direction={['column','row']} justifyContent='space-around' alignItems='center'>
             <Image src='https://qph.cf2.quoracdn.net/main-qimg-a60584595d47ece95e9c53714c16b3b1-lq'/>
             <Flex  direction='column' bg={'#CBB26A'} >
                 <Text fontSize='3em' font='bold'>Contact us </Text>
@@ -17,8 +17,8 @@ export default function Contact(){
                     </Flex>
 <Flex direction='column'>
 
-                    <Flex><Text>Contact</Text><Text>styleShoesByLior@gmail.com</Text></Flex>
-                    <Flex><Text>Based In</Text><Text>Rehovot, Israel</Text></Flex>         
+                    <Flex direction='column'><Text>Contact</Text><Text>styleShoesByLior@gmail.com</Text></Flex>
+                    <Flex direction='column'><Text>Based In</Text><Text>Rehovot, Israel</Text></Flex>         
 </Flex>
                 </Flex>
             </Flex>
